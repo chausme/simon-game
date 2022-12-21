9e1dc311bafb8a7c0694cba8619ff0cce0a4f881
Alex Chaus <chausme@gmail.com>
2022-12-21 15:02:56 +0400

    Add logic Round 3

--- src/simon.js
+++ src/simon.js
@@ -37,7 +37,91 @@ class Simon {
 
     #btnStrict = document.querySelector('#strict');
 
+    // Get color button since JS private properties can't be dynamic
+    #getColorBtn = color => {
+        if (color === 'green') {
+            return this.#btnGreen;
+        } else if (color === 'red') {
+            return this.#btnRed;
+        } else if (color === 'blue') {
+            return this.#btnBlue;
+        } else if (color === 'yellow') {
+            return this.#btnYellow;
+        }
+    };
+
+    // Reset highlighted color if any
+    #resetColor = () => {
+        console.log('color reset');
+        if (document.querySelector('.highlighted')) {
+            document.querySelector('.highlighted').classList.remove('highlighted');
+        }
+    };
+
+    // Flash particular color button
+    #flashColor = color => {
+        console.log(`flash color: ${color}`);
+        if (this.#sound) {
+            let audio = document.querySelector(`audio[data-color="${color}"]`);
+            audio.play();
+        }
+        this.#sound = true;
+        this.#getColorBtn(color).classList.add('highlighted');
+    };
+
+    // Process a single game turn
+    #gameTurn = () => {
+        this.#on = false;
+        console.log('game turn');
+
+        // Computer turn is completed
+        if (this.#flash === this.#turn) {
+            clearInterval(this.#intervalId);
+            this.#compTurn = false;
+            this.#resetColor();
+            this.#on = true;
+        }
+
+        // Process computer turn
+        if (this.#compTurn) {
+            this.#resetColor();
+            setTimeout(() => {
+                if (this.#order[this.#flash] === 1) {
+                    this.#flashColor('green');
+                } else if (this.#order[this.#flash] === 2) {
+                    this.#flashColor('red');
+                } else if (this.#order[this.#flash] === 3) {
+                    this.#flashColor('blue');
+                } else if (this.#order[this.#flash] === 4) {
+                    this.#flashColor('yellow');
+                }
+                this.#flash += 1;
+            }, 200);
+        }
+    };
+
+    // Start the game
+    #play = () => {
+        console.log('play');
+        // Reset a few values
+        this.#win = false;
+        this.#order = [];
+        this.#playerOrder = [];
+        this.#flash = 0;
+        this.#intervalId = null;
+        this.#turn = 1;
+        this.#turnCounter.textContent = 1;
+        this.#success = true;
+        // Get a list of random color turns
+        for (let i = 0; i < 20; i++) {
+            this.#order.push(Math.floor(Math.random() * 4) + 1);
+        }
+        this.#compTurn = true;
+        this.intervalId = setInterval(this.#gameTurn, 800);
+    };
+
     init() {
+        // Check for "on" status and update output accordingly
         this.#btnOn.addEventListener('change', e => {
             this.#on = e.currentTarget.checked;
             if (this.#on) {
@@ -46,6 +130,20 @@ class Simon {
             } else {
                 this.#btnStart.classList.add('disabled');
                 this.#turnCounter.value = '';
+                this.#resetColor();
+                clearInterval(this.#intervalId);
+            }
+        });
+
+        // Check for "strict" status
+        this.#btnStrict.addEventListener('change', e => {
+            this.#strict = e.currentTarget.checked;
+        });
+
+        // Start the game
+        this.#btnStart.addEventListener('click', () => {
+            if (this.#on || this.#win) {
+                this.#play();
             }
         });
     }
